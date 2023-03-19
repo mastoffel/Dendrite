b3f4518125869b51cbd98ed351c11839a79b3328
mastoffel <martin.adam.stoffel@gmail.com>
2023-03-19 15:28:22 +0000

    Initial Commit

---
+++ Dendrite/DendriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DendriteApp.swift
+//  Dendrite
+//
+//  Created by Martin Stoffel on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DendriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
